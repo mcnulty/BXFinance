3dae95f5eaece93e598781c2987e6e0af63b9579
rlyleping <rlyleping@gmail.com>
2020-11-23 15:39:52 -0700

    Update 50-before-post-start.sh

--- pingfederate/hooks/50-before-post-start.sh
+++ pingfederate/hooks/50-before-post-start.sh
@@ -27,3 +27,15 @@ mv /opt/out/instance/server/default/conf/META-INF/hivemodule.xml-modified /opt/o
 # cat /opt/out/instance/server/default/conf/META-INF/hivemodule.xml
 # echo "
 # ##########"
+
+# Delete bundled files so that Server Profile can apply newer ones
+echo Removing bundled files
+# AuthN API
+echo PF AuthN API
+rm -f /opt/out/instance/server/default/lib/pf-authn-api-sdk-1.0.0.48.jar
+rm -f /opt/out/instance/server/default/lib/pf-authn-api-sdk-1.0.0.54.jar
+echo PingID IK
+# PingID IK
+rm -f /opt/out/instance/server/default/deploy/pf-pingid-idp-adapter-2.6.jar
+rm -f /opt/out/instance/server/default/deploy/pf-pingid-quickconnection-1.0.1.jar
+rm -f /opt/out/instance/server/default/deploy/PingIDRadiusPCV-2.5.0.jar
